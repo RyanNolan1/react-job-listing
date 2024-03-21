2b57f11a2f69d5d40fd2a34dcdba89a69c09ef56
Ryan <ryannolan1990@hotmail.com>
2024-03-21 15:09:52 +0000

    this will get the array to appear in console

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-import './App.css';
+import "./App.css";
+import data from "./data.json";
 
 function App() {
-  return (
-   <div></div>
-  );
+  return ( <div>
+  <JobList />
+  {/* <Job /> */}
+  </div>
+  )
 }
+
+
+function JobList() {
+console.log(data)
+}
+
+// function Job({ data }) {
+//   return <div>Test</div>;
+// }
 
 export default App;
