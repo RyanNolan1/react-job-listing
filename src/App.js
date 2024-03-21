6ad583b96294605b22e7b4521901b2571908d81d
Ryan <ryannolan1990@hotmail.com>
2024-03-21 17:02:00 +0000

    this will set the languages when clicked and put them in an array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./App.css";
 import data from "./data.json";
+import { useState } from "react";
 
 function App() {
   return (
     <div>
+      <LanguageList />,
       <JobList />
     </div>
   );
 }
 
+function LanguageList() {}
+
 function JobList() {
+  let nextId = 0;
+
+  const [languages, setLanguages] = useState([]);
+
   return (
     <div>
       <ul className="jobs-list">
@@ -25,7 +33,20 @@ function JobList() {
             <hr></hr>
             <div className="button-container">
               {job.languages.map((language, index) => {
-               return <button key={index} className="language-buttons">{language}</button>
+                return (
+                  <button
+                    onClick={(e) => {
+                      setLanguages([
+                        ...languages,
+                        { id: nextId++, language: language },
+                      ]);
+                    }}
+                    key={index}
+                    className="language-buttons"
+                  >
+                    {language}
+                  </button>
+                );
               })}
             </div>
           </li>
