6cfbb537d7fc149597f65ea7fd608239fa486178
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2024-03-25 20:48:01 +0000

    this will fix some console errors

--- src/App.js
+++ src/App.js
@@ -67,19 +67,18 @@ function JobDetailsList({ jobDetails, onDeleteFilter }) {
       <div className="job-details-buttons-container">
         {jobDetails.map((jobDetail) => {
           return (
-            <div className="job-details-button-container">
+            <div className="job-details-button-container"   key={jobDetail.id}>
               <p className="job-details-button-text">{Object.values(jobDetail)[1]}</p>
               <button
                 className="job-details-button"
                 onClick={() => {
                   onDeleteFilter(jobDetail.id);
                 }}
-                key={jobDetail.id}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
                   <path
                     fill="#FFF"
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"
                   />
                 </svg>
@@ -108,7 +107,7 @@ function JobList({ onHandlejobDetails, onhandleFilter, filteredJobs }) {
               {job.location}
             </p>
             </div>
-            <span class="divider"></span>
+            <span className="divider"></span>
             <div className="button-container">
               <button
                 onClick={() => {
