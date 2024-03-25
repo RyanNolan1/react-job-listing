a5db8547a35def7f7a1f5cef0ee034501c00f0c8
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2024-03-25 20:12:09 +0000

    this will add further styling to all the buttons on the app

--- src/App.js
+++ src/App.js
@@ -63,25 +63,31 @@ function App() {
 function JobDetailsList({ jobDetails, onDeleteFilter }) {
   return (
     <div>
-    <header></header>
-    <div className="job-details-buttons-container">
-      {jobDetails.map((jobDetail) => {
-        return (
-          <div className="job-details-button-container">
-            {Object.values(jobDetail)[1]}
-          <button
-          className="job-details-button"
-            onClick={() => {
-              onDeleteFilter(jobDetail.id);
-            }}
-            key={jobDetail.id}
-          >
-            &#x2715;
-          </button>
-        </div>
-        );
-      })}
-    </div>
+      <header></header>
+      <div className="job-details-buttons-container">
+        {jobDetails.map((jobDetail) => {
+          return (
+            <div className="job-details-button-container">
+              <p className="job-details-button-text">{Object.values(jobDetail)[1]}</p>
+              <button
+                className="job-details-button"
+                onClick={() => {
+                  onDeleteFilter(jobDetail.id);
+                }}
+                key={jobDetail.id}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
+                  <path
+                    fill="#FFF"
+                    fill-rule="evenodd"
+                    d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
